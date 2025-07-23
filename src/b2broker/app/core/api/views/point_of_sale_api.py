317e7374a35dddd17a4f95a4bc6721584681a3db
Arif Garayev <garayevarif@gmail.com>
2025-07-23 09:52:15 +0400

    Transactions API & Service skeleton

--- src/b2broker/app/core/api/views/point_of_sale_api.py
+++ src/b2broker/app/core/api/views/point_of_sale_api.py
@@ -9,33 +9,57 @@ from rest_framework.response import (
     Response,
 )
 from rest_framework.views import APIView
+import logging
+from app.core.services import (
+    WalletService,
+    TransactionService,
+)
+from app.core.api.serializers import (
+    WalletCreateInputSerializer,
+    WalletCreateOutputSerializer,
+    WalletDetailInputSerializer,
+    WalletDetailOutputSerializer,
+    WalletTransactionOutputSerializer,
+    WalletUpdateInputSerializer,
+)
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiParameter,
+)
+from rest_framework import status, mixins, viewsets
+from dataclasses import dataclass
+from rest_framework_json_api import filters, django_filters
+from rest_framework_json_api.pagination import (
+    JsonApiPageNumberPagination,
+)
+from rest_framework import filters as drf_filters
 
 
-class TransactionCreateApi(
-    ServiceExceptionHandlerMixin,
-    APIView,
-): ...
+logger = logging.getLogger(__name__)
 
 
-class TransactionDetailReadApi(
-    ServiceExceptionHandlerMixin,
-    APIView,
-): ...
+@dataclass
+class WalletApiMeta:
+    OPENAPI_TAG = "Transaction"
 
 
-class TransactionListReadApi(
-    ServiceExceptionHandlerMixin,
-    APIView,
-): ...
+class TransactionApiBase(
+    ServiceExceptionHandlerMixin, APIView
+):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wallet_service: TransactionService = (
+            TransactionService(view=self)
+        )
 
 
-class TransactionUpdateApi(
-    ServiceExceptionHandlerMixin,
-    APIView,
-): ...
+class BalanceTransactionTopUpApi(TransactionApiBase): ...
 
 
-class TransactionDeleteApi(
-    ServiceExceptionHandlerMixin,
-    APIView,
+class BalanceTransactionDeductApi(TransactionApiBase): ...
+
+
+class BalanceTransactionWalletToWalletApi(
+    TransactionApiBase
 ): ...
--- src/b2broker/app/core/services/__init__.py
+++ src/b2broker/app/core/services/__init__.py
@@ -1,5 +1,4 @@
-from .wallet_service import (
-    WalletService,
-)
+from .wallet_service import WalletService
+from .transaction_service import TransactionService
 
-__all__ = ["WalletService"]
+__all__ = ["WalletService", "TransactionService"]
